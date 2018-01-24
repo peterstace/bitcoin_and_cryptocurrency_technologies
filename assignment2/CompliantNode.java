7d6299e97c5fd6fc4eb85c6556a6ba47170ddc64
Peter Stace <peterstace@gmail.com>
2018-01-24 17:51:30 +1100

    Add solution (80% mark)

--- assignment2/CompliantNode.java
+++ assignment2/CompliantNode.java
@@ -1,26 +1,32 @@
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.HashSet;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
+	private Set<Transaction> all;
+
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+		this.all = new HashSet<Transaction>();
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+		// NOP
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        this.all.addAll(pendingTransactions);
     }
 
     public Set<Transaction> sendToFollowers() {
         // IMPLEMENT THIS
+		return this.all;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+		for (Candidate c : candidates) {
+			this.all.add(c.tx);
+		}
     }
 }
